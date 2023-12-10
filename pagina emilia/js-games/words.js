32a6e69cb663bfda5c28f0d831fba465638666d4
Quokzie <153464931+Quokzie@users.noreply.github.com>
2023-12-10 17:29:59 -0500

    Add files via upload

---
+++ pagina emilia/js-games/words.js
@@ -0,0 +1,198 @@
+const wordList = [
+    {
+        word: "milo",
+        hint: "the name of one of your cats"
+    },
+    {
+        word: "guitar",
+        hint: "a musical instrument"
+    },
+    {
+        word: "aim",
+        hint: "a purpose or intention"
+    },
+    {
+        word: "venus",
+        hint: "planet of our solar system"
+    },
+    {
+        word: "gold",
+        hint: "a yellow precious metal"
+    },
+    {
+        word: "ebay",
+        hint: "online shopping site"
+    },
+    {
+        word: "antonia",
+        hint: "the name of the one who made this for you"
+    },
+    {
+        word: "coding",
+        hint: "related to programming"
+    },
+    {
+        word: "leemay",
+        hint: "ur avocado plushy"
+    },
+    {
+        word: "ray",
+        hint: "cat with gray fur"
+    },
+    {
+        word: "emilia",
+        hint: "a cutie patootie sweetheart"
+    },
+    {
+        word: "gif",
+        hint: "a file format for image (animated)"
+    },
+    {
+        word: "mental",
+        hint: "related to the mind"
+    },
+    {
+        word: "map",
+        hint: "diagram represent of an area"
+    },
+    {
+        word: "island",
+        hint: "land surrounded by water"
+    },
+    {
+        word: "hockey",
+        hint: "a famous outdoor game"
+    },
+    {
+        word: "chess",
+        hint: "related to an indoor game"
+    },
+    {
+        word: "instagram",
+        hint: "a social media app"
+    },
+    {
+        word: "snapchat",
+        hint: "social media app we met through"
+    },
+    {
+        word: "png",
+        hint: "a image file format"
+    },
+    {
+        word: "silver",
+        hint: "precious greyish-white metal"
+    },
+    {
+        word: "mobile",
+        hint: "an electronic device"
+    },
+    {
+        word: "han",
+        hint: "one of your favorite members of stray kids"
+    },
+    {
+        word: "dsmp",
+        hint: "thanks to what minecraft server did we begin speaking"
+    },
+    {
+        word: "google",
+        hint: "famous search engine"
+    },
+    {
+        word: "venice",
+        hint: "famous city of waters"
+    },
+    {
+        word: "excel",
+        hint: "microsoft product for windows"
+    },
+    {
+        word: "minho",
+        hint: "one of your favorite members of stray kids"
+    },
+    {
+        word: "nepal",
+        hint: "developing country name"
+    },
+    {
+        word: "flute",
+        hint: "a musical instrument"
+    },
+    {
+        word: "crypto",
+        hint: "related to cryptocurrency"
+    },
+    {
+        word: "spanish",
+        hint: "language you are trying to learn"
+    },
+    {
+        word: "mars",
+        hint: "planet of our solar system"
+    },
+    {
+        word: "antolia",
+        hint: "our names together"
+    },
+    {
+        word: "email",
+        hint: "related to exchanging message"
+    },
+    {
+        word: "male",
+        hint: "the gender of antonia's close friend"
+    },
+    {
+        word: "air",
+        hint: "related to a gas"
+    },
+    {
+        word: "idea",
+        hint: "a thought or suggestion"
+    },
+    {
+        word: "italian",
+        hint: "nationality of your ex"
+    },
+    {
+        word: "jagermeister",
+        hint: "strong alcohol you have taken"
+    },
+    {
+        word: "jpeg",
+        hint: "a image file format"
+    },
+    {
+        word: "search",
+        hint: "act to find something"
+    },
+    {
+        word: "key",
+        hint: "small piece of metal"
+    },
+    {
+        word: "egypt",
+        hint: "a country name"
+    },
+    {
+        word: "mario",
+        hint: "protagonist's name of a film based on a game"
+    },
+    {
+        word: "dubai",
+        hint: "developed country name"
+    },
+    {
+        word: "photo",
+        hint: "representation of person or scene"
+    },
+    {
+        word: "nile",
+        hint: "largest river in the world"
+    },
+    {
+        word: "rain",
+        hint: "related to a water"
+    },
+]
---
+++ pagina emilia/script-birthday.js
@@ -0,0 +1,5 @@
+document.getElementById('showButton').addEventListener('click', function() {
+    var imageContainer = document.getElementById('imageContainer');
+    imageContainer.style.display = 'block';
+  });
+  
